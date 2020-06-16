773ac03c2ba011ea169dc6a8aabd61133ad06532
davidsontampi71 <s11510110@student.unklab.ac.id>
2020-06-16 10:27:43 -0400

    Variable and Expression

---
+++ pertemuan3/variableandexpression/src/hello.java
@@ -0,0 +1,12 @@
+class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("welcome to multiplatform programming class");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber =  12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber ;
+        System.out.println(myTotal);
+    }
+}
